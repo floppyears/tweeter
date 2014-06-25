227b44c70071171ad0b7a1a7d7618713b27372b1
Colin Bradford <cjthehippo@gmail.com>
2014-06-25 15:15:39 -0700

    Added user model

---
+++ grails-app/domain/tweeter/User.groovy
@@ -0,0 +1,18 @@
+package tweeter
+
+class User {
+    int id
+    String username
+    String firstname
+    String lastname
+    String email
+
+
+    static constraints = {
+        id unique: true
+        username shared: "mustFill", size: 4..15
+        firstname shared: "mustFill"
+        lastname shared: "mustFill"
+        email shared: "mustFill", email: true
+    }
+}
