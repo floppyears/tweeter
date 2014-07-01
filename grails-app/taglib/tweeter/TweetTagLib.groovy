d97f223193b1eee7e1858b35092207bdf21ff61b
Colin Bradford <cjthehippo@gmail.com>
2014-07-01 15:53:04 -0700

    Added a tag to convert date to elapsed time

--- grails-app/taglib/tweeter/TweetTagLib.groovy
+++ grails-app/taglib/tweeter/TweetTagLib.groovy
@@ -1,5 +1,10 @@
 package tweeter
 
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
+
+import java.sql.Timestamp
+
 class TweetTagLib {
     //static defaultEncodeAs = [taglib:'html']
     static encodeAsForTags = [tagName: [taglib:'html'], alert: [taglib:'none']]
@@ -23,5 +28,22 @@ class TweetTagLib {
         if(!isNull) {
             out << render(template: "/tweet/alert", model: [alertType: alertType, msg: msg])
         }
+    }
+
+    def humanDate = { attrs, body ->
+        Timestamp tweetStamp = attrs.date
+        Date tweetDate = new Date(tweetStamp.getTime())
+        Date now = new Date()
+        TimeDuration timeSince = TimeCategory.minus(now, tweetDate)
+        if (timeSince.getDays() > 3)
+            out << tweetDate.format("MMMMM d, yyyy")
+        else if (timeSince.getDays() > 0)
+            out << timeSince.getDays() << "d"
+        else if (timeSince.getHours() > 0)
+            out << timeSince.getHours() << "h"
+        else if (timeSince.getMinutes() > 0)
+            out << timeSince.getMinutes() << "m"
+        else
+            out << timeSince.getSeconds() << "s"
     }
 }
--- grails-app/views/tweet/_tweet.gsp
+++ grails-app/views/tweet/_tweet.gsp
@@ -1,7 +1,7 @@
 <!-- Display a tweet -->
 <div class="panel panel-default">
     <div class="panel-heading">
-        <g:link action="show" id="${tweet.author.id}">${tweet.author.username}</g:link> @ ${tweet.dateCreated}
+        <g:link action="show" id="${tweet.author.id}">@${tweet.author.username}</g:link> - <g:humanDate date="${tweet.dateCreated}" />
     </div>
     <div class="panel-body">
         <p>${tweet.text}</p>
